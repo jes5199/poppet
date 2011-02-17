12133b46cb358f8ec34829bf0b89e2791f151efa
Jesse Wolfe <jes5199@gmail.com>
2011-02-17 02:12:29 -0800

    separate facts and announce

--- announce.rb
+++ announce.rb
@@ -1,36 +1,11 @@
-require 'rubygems'
-require 'json'
-require 'lib/http'
 require 'lib/execute'
+require 'lib/http'
 
 
 settings = {
   "server" => 'http://localhost:3000/inventory',
-  "facts" => './system_facts',
 }
 
-facts = {
-}
-
-#   system_facts/flat/*
-
-# TODO: refactor into two scripts (collection, announcement)
-Dir.glob( File.join( settings["facts"], "flat", "*" ) ) do |filename|
-  name = File.basename(filename).sub(/\..*/,'')
-  value = Poppet::Execute.execute(filename).chomp
-  facts[name] = value
-end
-
-#   system_facts/structured/*
-Dir.glob( File.join( settings["facts"], "structured", "*" ) ) do |filename|
-  json = Poppet::Execute.execute(filename)
-  # TODO validate schema
-  facts.merge!( JSON.parse( json ) )
-end
-
-p facts
-json = JSON.dump( facts )
-
-Poppet::HTTP.post(settings["server"], json)
+Poppet::HTTP.post(settings["server"], Poppet::Execute.execute('ruby system_facts.rb'))
 
 
---
+++ system_facts.rb
@@ -0,0 +1,30 @@
+require 'rubygems'
+require 'json'
+require 'lib/execute'
+
+
+settings = {
+  "facts" => './system_facts',
+}
+
+facts = {
+}
+
+#   system_facts/flat/*
+Dir.glob( File.join( settings["facts"], "flat", "*" ) ) do |filename|
+  name = File.basename(filename).sub(/\..*/,'')
+  value = Poppet::Execute.execute(filename).chomp
+  facts[name] = value
+end
+
+#   system_facts/structured/*
+Dir.glob( File.join( settings["facts"], "structured", "*" ) ) do |filename|
+  json = Poppet::Execute.execute(filename)
+  # TODO validate schema
+  facts.merge!( JSON.parse( json ) )
+end
+
+puts JSON.dump( facts )
+
+
+
