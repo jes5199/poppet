d2bed611e5ed0e4e42031cc0af1ecc42373a7ab6
Jesse Wolfe <jes5199@gmail.com>
2011-02-17 20:40:10 -0800

    map_files

--- lib/storage.rb
+++ lib/storage.rb
@@ -8,7 +8,13 @@ module Poppet
     end
 
     def self.timestamp_file(dir, &blk)
-      self.file(File.join(dir, Poppet::Timestamp.now), &blk)
+      self.file(File.join(dir, 'by_time', Poppet::Timestamp.now), &blk)
     end
+
+    def self.map_files( glob, filter, target_dir )
+      Dir.glob( File.join( glob ) ) do |input_filename|
+      end
+    end
+
   end
 end
