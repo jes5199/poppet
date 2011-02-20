3e004b5af0cdd09d3a94e05ae9c7f759a9e083a0
Jesse Wolfe <jes5199@gmail.com>
2011-02-20 00:48:02 -0800

    wip: don't pass ids to providers

--- lib/policy/applier.rb
+++ lib/policy/applier.rb
@@ -3,7 +3,7 @@ module Poppet
   class Policy::Applier < Policy
     def each
       # TODO: frontier walking
-      resources.each do |resource|
+      resources.each do |id, resource|
         yield(resource)
       end
     end
