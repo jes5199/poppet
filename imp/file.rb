32820702393786775db110a4f2756e72379cab51
Jesse Wolfe <jes5199@gmail.com>
2011-02-20 23:13:29 -0800

    wip: sketching

--- imp/file.rb
+++ imp/file.rb
@@ -24,6 +24,7 @@ system = Poppet::Implementor::Reader.new({
   "path" => {
     "read" => lambda { desired["path"] }
   },
+
   "exists" => {
     "read" => lambda { execute_test( "test", "-e", desired["path"] ) },
 
@@ -53,15 +54,6 @@ system = Poppet::Implementor::Reader.new({
   }
 })
 
-p system["path"]
-p system["checksum"]
-exit
-
-
-# TODO For "check", return if it matches desired
-
-# TODO For "survey", return what was on disk (for specified attrs)
-
 def write_file( path, content )
   #TODO: sudo
   #TODO: umode
@@ -69,26 +61,88 @@ def write_file( path, content )
   set( "content", content )
 end
 
-# state machine
-change( {"exists" => literal(true) }, {"exists" => literal(false) } ) do
-  # in traditional unix style, let's remove files and symlinks but not directories.
-  execute( "rm", desired["path"] )
-end
+writer = Poppet::Implementor::Writer.new([ # state machine
+  [
+    {"exists" => ["literal", true ] },
+    {"exists" => ["literal", false] },
+    lambda do |w, actual, desired|
+      w.really{
+        execute( "rm", desired["path"] ) # in traditional unix style, let's remove files and symlinks but not directories.
+      }
+      { "exists" => false }
+    end
+  ],
+  [
+    {"exists" => ["literal", false]},
+    {"exists" => ["literal", true ]},
+    lambda do |w, actual, desired|
+      w.really {
+        write_file( desired )
+      }
+      {
+        "exists"  => true,
+        "path"    => desired["path"],
+        "mode"    => desired["mode"],
+        "owner"   => desired["owner"],
+        "content" => desired["content"]
+      }
+    end
+  ],
 
-change( {"exists" => literal(false) }, {"exists" => literal(true) } ) do
-  write_file( "echo", desired["content"], desired["path"] )
-end
+  [
+    { "exists" => ["literal", true], "content" => "string" },
+    { "exists" => ["literal", true], "content" => "string" },
+    lambda do |w, actual, desired|
+      w.really {
+        write_file( "echo", desired["content"], desired["path"] )
+      }
+      { "content" => desired["content"] }
+    end
+  ],
 
-within( {"exists" => literal(true)} ) do
-  change( { "content" => string } , { "content" => string } ) do
-    write_file( "echo", desired["content"], desired["path"] )
-  end
+  [
+    { "exists" => ["literal", true], "mode" => "string" },
+    { "exists" => ["literal", true], "mode" => "string" },
+    lambda do |w, actual, desired|
+      w.really {
+        execute( "chmod", desired["mode"], desired["path"] )
+      }
+      {
+        "mode"    => simulated_chmod( actual["mode"], desired["mode"] ),
+      }
+    end
+  ],
 
-  change( { "mode" => string }, { "mode" => string } ) do
-    execute( "chmod", desired["mode"], desired["path"])
-  end
+  [
+    { "exists" => ["literal", true], "owner" => "string" },
+    { "exists" => ["literal", true], "owner" => "string" },
+    lambda do |w, actual, desired|
+      return {} if numeric_user( desired["owner"] ) == numeric_user( actual["owner"] )
+      w.really {
+        execute( "chown", desired["owner"], desired["path"] )
+      }
+      {
+        "owner" => desired["owner"]
+      }
+    end
+  ],
 
-  change( { "owner" => either(string, integer) }, { "owner" => either(string, integer) } ) do
-    execute( "chown", desired["owner"], desired["path"])
-  end
-end
+  [
+    { "exists" => ["literal", true], "group" => "string" },
+    { "exists" => ["literal", true], "group" => "string" },
+    lambda do |w, actual, desired|
+      return {} if numeric_group( desired["group"] ) == numeric_group( actual["group"] )
+      w.really {
+        execute( "chown", desired["group"], desired["path"] )
+      }
+      {
+        "group" => desired["group"]
+      }
+    end
+  ]
+])
+
+writer.send()
+# TODO For "check", return if it matches desired
+# TODO For "survey", return what was on disk (for specified attrs)
+
