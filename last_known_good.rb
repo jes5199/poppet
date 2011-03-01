188dfe7349891e81fb35423d26cb0d4ac994e61e
Jesse Wolfe <jes5199@gmail.com>
2011-03-01 00:28:25 -0800

    generic last_known_good getter

---
+++ last_known_good.rb
@@ -0,0 +1,25 @@
+require 'lib/http'
+require 'lib/execute'
+require 'lib/storage'
+require 'lib/settings'
+
+settings = Poppet::Settings.new
+
+include Poppet::Execute::EscapeWithLittleE
+
+url = ARGV[0] #TODO: validate command lines
+filename = url.sub('http://', '')
+filename = File.join(settings['last_known_good'], filename )
+Poppet::Storage.make_dir_for( filename )
+content = nil
+begin
+  content = Poppet::HTTP.get( url )
+  print content
+rescue => e
+  STDERR.puts e
+end
+if content
+  File.open( filename, "w" ) {|f| f.print content }
+else
+  puts File.read( filename )
+end
