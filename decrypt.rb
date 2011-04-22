d74064a3c2598228237a2bb5c2a6b7aff26eb225
Jesse Wolfe <jes5199@gmail.com>
2011-04-22 15:53:46 -0700

    I just noticed that decrypt and unsign are the same

--- decrypt.rb
+++ decrypt.rb
@@ -1,6 +1,6 @@
 # TODO: settingsify
 # TODO: gather key ID
-Kernel.open("| gpg -u 0x90605AF0 -d --passphrase poppet --secret-keyring keys/local.sec --keyring keys/local.pub --yes -", 'w') do |gpg|
+Kernel.open("| gpg -u 0x90605AF0 --decrypt --passphrase poppet --secret-keyring keys/local.sec --keyring keys/local.pub --yes -", 'w') do |gpg|
   STDIN.each do |line|
     gpg.print line
   end
