c9661d124c0a657c2690ffd231141bf55fd18fa9
Jesse Wolfe <jes5199@gmail.com>
2011-05-29 23:50:50 -0700

    Put DSL into a sandbox rather than main

--- lib/implementor/dsl.rb
+++ lib/implementor/dsl.rb
@@ -7,21 +7,23 @@ require 'lib/implementor/checker'
 require 'lib/implementor/writer'
 require 'lib/implementor/solver'
 
-include Poppet::Execute::EscapeWithLittleE
-include Poppet::Execute::ExecuteWithLittleX
+class Poppet::Implementor::DSLSandbox
+  include Poppet::Execute::EscapeWithLittleE
+  include Poppet::Execute::ExecuteWithLittleX
 
-class << self
-  attr_writer :reader_class, :writer_class, :checker_class
+  attr_accessor :reader_class, :writer_class, :checker_class
 end
+
 def implementor
   command, desired_json = JSON.parse( STDIN.read )
   desired = Poppet::Resource.new( desired_json )
 
-  yield(desired)
+  box = Poppet::Implementor::DSLSandbox.new
+  box.instance_eval{ yield(desired) }
 
-  reader = @reader_class.new(desired)
-  checker = @checker_class.new
-  writer = @writer_class.new(desired)
+  reader = box.reader_class.new(desired)
+  checker = box.checker_class.new
+  writer = box.writer_class.new(desired)
 
   puts Poppet::Implementor::Solver.new( desired, reader, checker, writer ).do( command ).to_json
 end
